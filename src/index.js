057ba9cbf236efb8c19b77f14cbba9694119134e
unknown <mg-milan@hotmail.rs>
2021-11-17 18:28:23 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoContainer from './components/TodoContainer';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <TodoContainer />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
